602637b5bf8459fc0b5c9f57cf10486bc363dad9
saniyaagrawal <saniya.agrawal30@gmail.com>
2021-05-29 23:42:09 +0530

    profile page completed

--- src/screens/AdminProfile.js
+++ src/screens/AdminProfile.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text,Image } from 'react-native'
 import { styles as common } from '../styles/common'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const AdminProfile = () => {
     return (
-        <View style={{ flexDirection:'row',alignItems:'center',justifyContent:'space-between',marginLeft: 2}} >
-            <View style={{ flexDirection:'row',alignItems:'center'}}>
-                <Text style={common.fontSmallBold}>Name </Text>
+        <View>
+            <Image source={require("../assets/images/restaurant.jpeg")} style={{ height: '70%', width: '100%'}} />
+            <View style={{ marginLeft:'80%',marginTop:'-4%'}} >
+                <MaterialIcons name="food-bank" color='#F2A253' size={60} />
+                <Text style={{ fontSize:11}}>10 AM-10 PM</Text>
             </View>
-            <Text style={[common.fontSmallBold,{marginRight:'3%'}]}>Price</Text>
+            <View style={{marginHorizontal:15,marginTop:'7%'}}>
+                <Text style={[common.fontLarge,{marginTop:'-20%'}]}>Grande Restaurant</Text>
+                <Text style={[common.secondaryText,{fontSize:15}]}>Indore, MP</Text>
+                <Text style={[common.secondaryText,{fontSize:15,marginTop:5}]}>+919876543210</Text>
+                <Text style={{marginTop:24,fontSize:14}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</Text>
+            </View>
         </View>
     )
 }
